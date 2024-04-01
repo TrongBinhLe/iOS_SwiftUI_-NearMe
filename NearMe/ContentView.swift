3e6ba78a95787943080f48904a238b9523cdab95
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-04-01 10:48:34 +0700

    Implement location manager

--- NearMe/ContentView.swift
+++ NearMe/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var locationManager = LocationManager()
     var body: some View {
         MapView()
     }
--- NearMe/Coordinators/Coordinator.swift
+++ NearMe/Coordinators/Coordinator.swift
@@ -14,4 +14,15 @@ final class Coordinator: NSObject, MKMapViewDelegate {
     init(control: MapView) {
         self.control = control
     }
+    
+    func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+        if let annotationView = views.first {
+            if let annotaion = annotationView.annotation {
+                if annotaion is MKUserLocation {
+                    let region  = MKCoordinateRegion(center: annotaion.coordinate, latitudinalMeters: 100, longitudinalMeters: 100)
+                    mapView.setRegion(region, animated: true)
+                }
+            }
+        } 
+    }
 }
